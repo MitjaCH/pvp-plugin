a0a56604d4e6142dd9b1602a9bbcdcf1757e7ffc
MitjaCH <kurathmitja@gmail.com>
2024-07-22 16:04:43 +0200

    [UPDATE] Improved looks of sidebar aswell as decreased lockDuration for testing

--- src/main/java/online/mitja/noElytraNetherite/commands/SidebarCommand.java
+++ src/main/java/online/mitja/noElytraNetherite/commands/SidebarCommand.java
@@ -68,13 +68,16 @@ public class SidebarCommand implements CommandExecutor, TabCompleter {
     private void showSidebar(Player player) {
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         Scoreboard board = manager.getNewScoreboard();
-        Objective objective = board.registerNewObjective("sidebar", "dummy", ChatColor.GOLD + "§l§nServer Info");
+        Objective objective = board.registerNewObjective("sidebar", "dummy", ChatColor.GOLD + "" + ChatColor.BOLD + "§l§nServer Info");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        Score header = objective.getScore(ChatColor.DARK_GREEN.toString() + ChatColor.BOLD + "Teams and Kills");
-        header.setScore(9);
+        int index = 15;
 
-        int index = 8;
+        // Title Header
+        Score header = objective.getScore(ChatColor.DARK_GREEN.toString() + ChatColor.BOLD + "Teams and Kills");
+        header.setScore(index--);
+
+        // Teams and Kills
         for (Team team : Bukkit.getScoreboardManager().getMainScoreboard().getTeams()) {
             String teamName = team.getName();
             int kills = teamKills.getOrDefault(teamName, 0);
@@ -82,25 +85,43 @@ public class SidebarCommand implements CommandExecutor, TabCompleter {
             score.setScore(index--);
         }
 
+        // Spacing
         Score separator1 = objective.getScore(ChatColor.GRAY + "---------------");
-        separator1.setScore(7);
+        separator1.setScore(index--);
 
+        // PvP/End Time Remaining
         long timeRemaining = (plugin.getConfig().getLong("lockDuration") - (System.currentTimeMillis() - plugin.getConfig().getLong("startTime"))) / 1000;
         int hours = (int) (timeRemaining / 3600);
         int minutes = (int) ((timeRemaining % 3600) / 60);
         int seconds = (int) (timeRemaining % 60);
 
         String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        Score timeScore = objective.getScore(ChatColor.YELLOW + "PvP/End: " + ChatColor.WHITE + timeFormatted);
-        timeScore.setScore(6);
+        Score pvpInfo = objective.getScore(ChatColor.YELLOW + "PvP & End restrictions");
+        pvpInfo.setScore(index--);
+        Score timeScore = objective.getScore(ChatColor.YELLOW + "disabled in: " + ChatColor.WHITE + timeFormatted);
+        timeScore.setScore(index--);
 
+        // More Spacing
         Score separator2 = objective.getScore(ChatColor.GRAY + "---------------");
-        separator2.setScore(5);
+        separator2.setScore(index--);
 
+        // Player Count
+        Score playerCount = objective.getScore(ChatColor.GREEN + "Online Players: " + ChatColor.WHITE + Bukkit.getOnlinePlayers().size());
+        playerCount.setScore(index--);
+
+        // More Information
+        Score serverMOTD = objective.getScore(ChatColor.GOLD + "MOTD: " + ChatColor.WHITE + Bukkit.getMotd());
+        serverMOTD.setScore(index--);
+
+        // More Spacing
+        Score separator3 = objective.getScore(ChatColor.GRAY + "---------------");
+        separator3.setScore(index--);
+
+        // Server IP
         Score footer1 = objective.getScore(ChatColor.AQUA + "Server IP:");
-        footer1.setScore(4);
+        footer1.setScore(index--);
         Score footer2 = objective.getScore(ChatColor.LIGHT_PURPLE.toString() + ChatColor.BOLD + "mc.mitja.online");
-        footer2.setScore(3);
+        footer2.setScore(index--);
 
         player.setScoreboard(board);
     }
